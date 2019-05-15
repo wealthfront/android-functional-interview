9a3f140220e7b4014035a3d4e7dd8fb23ed40e59
Chris Mathew <chrismathew@wealthfront.com>
2019-05-15 10:52:10 -0700

    First commit

---
+++ app/src/main/java/com/wealthfront/android_functional_interview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wealthfront.android_functional_interview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
