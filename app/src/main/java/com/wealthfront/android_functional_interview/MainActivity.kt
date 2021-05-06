852be155d2d4923276aad3eb5429d0a5aeb9ac68
Chris Mathew <chrismathew@wealthfront.com>
2021-05-06 11:58:22 -0700

    use interview package name

--- app/src/main/java/com/wealthfront/android_functional_interview/MainActivity.kt
+++ app/src/main/java/com/wealthfront/android_functional_interview/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.wealthfront.android_functional_interview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
