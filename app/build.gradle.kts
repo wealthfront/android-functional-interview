7aae1580f06a8218a4be8b562daa9bbcb41854f3
cmathew <chris.w.mathew@gmail.com>
2024-04-16 13:09:45 -0700

    exclude java compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,10 +26,12 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
+
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
   }
+
   flavorDimensions += listOf("language", "ui")
   productFlavors {
     register("java") {
@@ -51,15 +53,27 @@ android {
       dimension = "ui"
     }
   }
+
+  androidComponents {
+    beforeVariants { variantBuilder ->
+      if (variantBuilder.productFlavors.containsAll(listOf("language" to "java", "ui" to "compose"))) {
+        variantBuilder.enable = false
+      }
+    }
+  }
+
   kotlinOptions {
     jvmTarget = "1.8"
   }
+
   buildFeatures {
     compose = true
   }
+
   composeOptions {
     kotlinCompilerExtensionVersion = "1.5.1"
   }
+
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
